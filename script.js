c22a443aea678542b885c0e883344bd151727cd8
Zviad Karkashadze <85183221+zviadkarkashadze@users.noreply.github.com>
2021-11-02 23:30:51 +0400

    added some sum operation

---
+++ script.js
@@ -0,0 +1,32 @@
+//1
+// function sum(...args) {
+//   let numbersSum = 0;
+
+//   for (let item of args) {
+//     if (item > 0){
+//    numbersSum += item;
+//    }
+//   } 
+
+//   console.log(numbersSum);
+// }
+
+// sum(2, -9, 5, 11, -30, 100, -8, -11, -4, 5, -6, 8);
+
+
+
+//2
+// function sum1(...args1) {
+//   let numsum = 0;
+
+//   for (let item1 of args1) {
+//     numsum += item1;
+//   }
+//   console.log(numsum);
+// }
+
+// sum1(10, 50, 6, 7, 8, 11, 6, 3, 9);
+
+
+//3
+
